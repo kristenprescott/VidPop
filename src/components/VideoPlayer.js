7dfd13d7d27d0e70188b0e27347b14430172b765
KayPea <prescottdarling@gmail.com>
2021-07-13 07:24:12 -0400

    added Options component, video does currently get feedback

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,16 +1,21 @@
-import { Grid, Typography, Paper } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useContext } from "react";
+import { Grid, Typography, Paper, makeStyles } from "@material-ui/core";
+// import { makeStyles } from "@material-ui/core/styles";
 import { SocketContext } from "../SocketContext";
 
 const useStyles = makeStyles((theme) => ({
   video: {
-    // width: "550px",
-    width: "45vmax",
-    width: "42vmax",
-    height: "48vmin",
+    // width: "38vmax",
+    // height: "30vmin",
+    width: "600px",
     [theme.breakpoints.down("xs")]: {
       width: "300px",
     },
+  },
+  bigVideo: {
+    // width: "48vmax",
+    // height: "40vmin",
+    width: "600px",
   },
   gridContainer: {
     justifyContent: "center",
@@ -27,40 +32,74 @@ const useStyles = makeStyles((theme) => ({
 
 const VideoPlayer = () => {
   const classes = useStyles();
+  const {
+    name,
+    callAccepted,
+    myVideo,
+    userVideo,
+    callEnded,
+    stream,
+    call,
+  } = useContext(SocketContext);
+
+  const handleClick = () => {
+    console.log("name: ", name);
+    console.log("callAccepted: ", callAccepted);
+    console.log("myVideo: ", myVideo);
+    console.log("userVideo: ", userVideo);
+    console.log("callEnded: ", callEnded);
+    console.log("stream: ", stream);
+    console.log("call: ", call);
+  };
 
   return (
     <Grid container className={classes.gridContainer}>
       {/* OUR OWN VIDEO */}
-      <Paper className={classes.paper}>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h5" gutterBottom>
-            Name
-          </Typography>
-          <video
-            playsInline
-            muted
-            // ref={myVideo}
-            ref={null}
-            autoPlay
-            className={classes.video}
-          />
-        </Grid>
-      </Paper>
+      {stream && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>
+              {name || "no username"}
+            </Typography>
+            <video
+              controls
+              playsInline
+              muted
+              ref={myVideo}
+              // autoPlay
+              // className={classes.video}
+              className={!stream ? classes.video : classes.bigVideo}
+            >
+              Sorry, your browser doesn't support embedded videos :(
+            </video>
+          </Grid>
+        </Paper>
+      )}
       {/* USER'S VIDEO */}
-      <Paper className={classes.paper}>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h5" gutterBottom>
-            Name
-          </Typography>
-          <video
-            playsInline
-            // ref={userVideo}
-            ref={null}
-            autoPlay
-            className={classes.video}
-          />
-        </Grid>
-      </Paper>
+      {callAccepted && !callEnded && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>
+              {call.name || "no username"}
+            </Typography>
+            <video
+              playsInline
+              ref={userVideo}
+              autoPlay
+              className={classes.video}
+            >
+              Sorry, your browser doesn't support embedded videos :(
+            </video>
+          </Grid>
+        </Paper>
+      )}
+
+      {/* <button
+        onClick={handleClick}
+        style={{ border: "1px solid tomato", padding: "30px 50px" }}
+      >
+        Clicky
+      </button> */}
     </Grid>
   );
 };
